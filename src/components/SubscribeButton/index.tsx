d1b6bf0e23a133cbf0082e9d4a73513aea87aba6
Frederico Stevanato Rocha <fredzstevanato@gmail.com>
2021-03-23 23:24:24 -0300

    feat: add payment with stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,12 +2,37 @@ import styles from './styles.module.scss'
 
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { useSession, signIn } from 'next-auth/client'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 export function SubscribeButton () {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return(
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
